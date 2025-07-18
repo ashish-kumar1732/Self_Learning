d25bc44480f8524bb3c423a5d9232c430010c20a
Ashish <216260904+ashish-kumar1732@users.noreply.github.com>
2025-07-19 00:50:47 +0530

    change author email-id

--- Code With Harry/Practice_1/squared_num.py
+++ Code With Harry/Practice_1/squared_num.py
@@ -1,8 +1,9 @@
 squared_num = {x:x**2 for x in range(1,11)}
-print(squared_num)
+# print(squared_num)
 
 
 ### Another way to print this ###
 
 for i in squared_num:
     print(f"Square of {i} is {i**2}")
+
