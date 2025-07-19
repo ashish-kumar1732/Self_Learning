001d8ab6b8e23dd602c5fda0e2c30af79e11f80e
Ashish <216260904+ashish-kumar1732@users.noreply.github.com>
2025-07-19 11:53:14 +0530

    add conditional practice

---
+++ Code With Harry/Practice_1/key_set.py
@@ -0,0 +1,6 @@
+keys = ["ash","ashi","ashu","ashly","ashish"]
+default_value = "same"
+new_dict = dict.fromkeys(keys,default_value)
+print(new_dict)
+new_dict = dict.fromkeys(keys,keys)
+print(new_dict)
