758b73b8897c1f84d80b0ba8b9c9ccaa16561792
Ashish <216260904+ashish-kumar1732@users.noreply.github.com>
2025-08-22 15:26:59 +0530

    Add Oops

---
+++ Code With Harry/Oops/oops1.py
@@ -0,0 +1,26 @@
+# class student: ---- class ----
+#     name = "ash"
+
+# ---- instance of the class -----
+
+# ashi = student() ---- Object1 ----
+# ashish = student() ---- Object2 ----
+
+# ashish.name = "ashish"
+# ashish.std = 12
+# ashish.section = "a"
+# print(ashish.name, ashish.section, ashish.std)
+
+
+# ---- instance of the class -----
+
+# class student():
+#     pass
+
+# sahi= student() ---- Object1 ----
+# sahi2= student() ---- Object2 ----
+
+# sahi.name = "ashish"
+# sahi2.name = "ash"
+# print(sahi2.name)
+
